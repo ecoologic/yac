3777b32d9a7503f500c4f5cee9614e195c881789
erik <erikecoologic@gmail.com>
2014-06-22 23:10:42 +1000

    watch messages and set avatar

--- app/js/app.js
+++ app/js/app.js
@@ -15,7 +15,24 @@ directives.authentication = function(Authentication) {
 };
 var controllers = {}; /////////////////////////////////////////////////////////
 controllers.MessagesCtrl = function($scope, Resource) {
-  $scope.users    = Resource.users;
+  var setMessageSenderUserAvatarUrl = function(messageKey) {
+    var message = $scope.messages[messageKey];
+    if(!message.senderUserAvatarUrl) {
+      var path = 'users/' + message.senderUserKey + '/thirdPartyUserData/avatar_url';
+      var ref = Resource.ref(path);
+      ref.on('value', function(snapshot) {
+        $scope.messages[messageKey].senderUserAvatarUrl = snapshot.val() || 'images/missing_avatar.png?';
+      });
+    }
+  }
+  $scope.$watchCollection('messages', function(newMessages, oldMessages) {
+    if(newMessages) {
+      _.each(newMessages.$getIndex(), function(messageKey) {
+        setMessageSenderUserAvatarUrl(messageKey);
+      });
+    };
+  });
+
   $scope.messages = Resource.messages;
 
   $scope.delete = function(key) {
@@ -23,11 +40,8 @@ controllers.MessagesCtrl = function($scope, Resource) {
   }
 };
 controllers.NewMessageCtrl = function($scope, Resource, Authentication) {
-  $scope.currentUserKey = Authentication.currentUserKey;
-  $scope.currentUser    = Authentication.currentUser;
-
   $scope.create = function() {
-    $scope.newMessage.senderUserKey = currentUserKey;
+    $scope.newMessage.senderUserKey = Authentication.currentUserKey;
     $scope.newMessage.createdAt = (new Date()).toLocaleString();
     Resource.messages.$add($scope.newMessage);
     $scope.newMessage = {};
